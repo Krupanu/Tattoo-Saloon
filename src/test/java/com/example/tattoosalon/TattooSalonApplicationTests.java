dff9a3a92d46bb2f494d7ad5b1713989832894f1
”Krupanu” <Krupanu2@gmail.com>
2025-04-22 18:51:43 +0300

    added User, roles, contactDetails and history

---
+++ src/test/java/com/example/tattoosalon/TattooSalonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tattoosalon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TattooSalonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
